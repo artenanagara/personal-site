9ab572c7bfeb56d6e621e990bd728c67762d1645
Artena Nagara <artenanagara22@gmail.com>
2026-06-28 00:06:57 +0700

    Add new profile image in WEBP format

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -58,7 +58,7 @@ const motionStagger = (delay = 0) => ({
           :enter="{ opacity: 1, scale: 1, transition: { ...transitionMain, delay: 400 } }"
           class="w-full lg:w-80 h-80 bg-[#d9d9d9] shrink-0 rounded-none shadow-sm origin-center"
         >
-            <img src="../assets/images/photo-me.PNG" alt="" class="grayscale"></img>
+            <img src="../assets/images/photo-me.webp" alt="" class="grayscale"></img>
         </div>
       </div>
 
@@ -71,7 +71,7 @@ const motionStagger = (delay = 0) => ({
           :visible-once="{ opacity: 1, y: 0, transition: { ...transitionMain } }"
           class="w-full h-screen bg-[#d9d9d9] rounded-none mb-12 shadow-sm"
         >
-             <img src="../assets/images/about-me.png" alt="" class="w-full h-full object-cover">
+             <img src="../assets/images/about-me.webp" alt="" class="w-full h-full object-cover">
         </div>
 
         <div class="flex flex-col lg:flex-row gap-16 justify-between items-start">
